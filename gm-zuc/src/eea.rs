6df321fa6b973c3971265ff867e7d6c78940a7ab
crayfishigo <evanyang1120@163.com>
2023-04-19 13:18:46 +0800

    fix EEA encrypt

--- gm-zuc/src/eea.rs
+++ gm-zuc/src/eea.rs
@@ -24,30 +24,53 @@ impl EEA {
         EEA { zuc }
     }
 
-    pub fn encrypt(&mut self, msg: &[u8], ilen: u32) -> Vec<u8> {
-        let zero_bits = ilen & 0x7;
+    pub fn encrypt(&mut self, msg: &[u32], ilen: u32) -> Vec<u32> {
         let mut rs = vec![];
         let keylength = (ilen + 31) / 32;
-        let len = ilen >> 3;
 
         let keys = self.zuc.generate_keystream(keylength as usize);
         let keys = keys.as_slice();
         for i in 0..keylength as usize {
-            let mut j = 0;
-            while j < 4 && (i * 4 + j) < len as usize {
-                rs[4 * i + j] = msg[4 * i + j] ^ (((&keys[i] >> (8 * (3 - j))) & 0xff) as u8);
-                j += 1;
-            }
-            rs[i] ^= msg[i];
+            rs.push(msg[i] ^ keys[i]);
         }
 
-        if zero_bits > 0 {
-            rs[len as usize - 1] = rs[len as usize - 1] & (((0xff) << (8 - zero_bits)) as u8)
+        if ilen % 32 != 0 {
+            rs[keylength as usize - 1] &= 0xffffffff << (32 - (ilen % 32));
         }
 
-        for k in len as usize..rs.len() {
-            rs[k] =0;
-        }
         rs
+    }
+}
+
+#[cfg(test)]
+mod eea_test {
+    use crate::eea::EEA;
+
+    #[test]
+    fn test_eea() {
+        let ck: [u8; 16] = [
+            0x17, 0x3d, 0x14, 0xba, 0x50, 0x03, 0x73, 0x1d, 0x7a, 0x60, 0x04, 0x94, 0x70, 0xf0,
+            0x0a, 0x29,
+        ];
+
+        let count = 0x66035492_u32;
+        let bearer = 0xf_u32;
+        let direction = 0_u32;
+        let length = 0xc1_u32;
+
+        let ibs: [u32; 7] = [
+            0x6cf65340, 0x735552ab, 0x0c9752fa, 0x6f9025fe, 0x0bd675d9, 0x005875b2, 0x00000000,
+        ];
+
+        let obs: [u32; 7] = [
+            0xa6c85fc6, 0x6afb8533, 0xaafc2518, 0xdfe78494, 0x0ee1e4b0, 0x30238cc8, 0x00000000,
+        ];
+
+        let mut eea = EEA::new(&ck, count, bearer, direction);
+        let rs = eea.encrypt(&ibs, length);
+        println!("{:?}", rs);
+        for z in rs {
+            print!("{:x},", z)
+        }
     }
 }
